db0e0630c355228290478b63c7f2cf68e091fb86
Klaus Matzka <klaus.h.matzka@gmail.com>
2023-10-16 09:18:06 +0200

    Initial Commit

---
+++ NavigationSplitViewSample/NavigationSplitViewSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationSplitViewSampleApp.swift
+//  NavigationSplitViewSample
+//
+//  Created by Klaus Matzka on 16.10.23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationSplitViewSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
